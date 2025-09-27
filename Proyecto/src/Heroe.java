db112d560902d898ca77936a3fef217eea75ce9f
TheKuuma <juan.c.quintero.g@correounivalle.edu.co>
2025-09-27 10:49:24 -0500

    Update Heroe.java
    Ortografía

--- Proyecto/src/Heroe.java
+++ Proyecto/src/Heroe.java
@@ -1,5 +1,5 @@
 public class Heroe extends Personaje {
-    public Heroe (String nombre, int HP, int MP, int ataque, int defensa, int velociadad ){
-        super(nombre, HP, MP, ataque, defensa, velociadad);
+    public Heroe (String nombre, int HP, int MP, int ataque, int defensa, int velocidad ){
+        super(nombre, HP, MP, ataque, defensa, velocidad);
     } 
 }
