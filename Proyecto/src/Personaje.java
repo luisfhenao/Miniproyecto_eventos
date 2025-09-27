805820dc885fe9c2a3357f119ed5e4b32563b74b
TheKuuma <juan.c.quintero.g@correounivalle.edu.co>
2025-09-27 10:49:47 -0500

    Update Personaje.java
    Ortografía

--- Proyecto/src/Personaje.java
+++ Proyecto/src/Personaje.java
@@ -4,15 +4,15 @@ public abstract class Personaje {
     protected int MP;
     protected int ataque;
     protected int defensa;
-    protected int velociadad;
+    protected int velocidad;
 
-    public Personaje(String nombre, int HP, int MP, int ataque, int defensa, int velociadad ){
+    public Personaje(String nombre, int HP, int MP, int ataque, int defensa, int velocidad ){
         this.nombre = nombre;
         this.HP = HP;
         this.MP = MP;
         this.ataque = ataque;
         this.defensa = defensa;
-        this.velociadad = velociadad;
+        this.velociadad = velocidad;
     }
     
 }
