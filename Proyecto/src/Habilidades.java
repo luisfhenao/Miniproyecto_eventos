1834731d1816228f11d02f24ebf676904482c0b5
TheKuuma <juan.c.quintero.g@correounivalle.edu.co>
2025-10-05 20:41:42 -0500

    Update Habilidades.java

--- Proyecto/src/Habilidades.java
+++ Proyecto/src/Habilidades.java
@@ -1,3 +1,23 @@
-public abstract class Habilidades {
-    
+public abstract class Habilidad {
+    protected String nombre;
+    protected int costoMP;
+
+    public Habilidad(String nombre, int costoMP) {
+        this.nombre = nombre;
+        this.costoMP = costoMP;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getCostoMP() {
+        return costoMP;
+    }
+
+    public boolean puedeUsar(Personaje usuario) {
+        return usuario.getMP() >= costoMP;
+    }
+
+    public abstract void aplicar(Personaje usuario, Personaje objetivo);
 }
