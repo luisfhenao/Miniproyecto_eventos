8c4c8d72d051eff8f0dbc8e354672cb48e143332
TheKuuma <juan.c.quintero.g@correounivalle.edu.co>
2025-10-05 22:00:20 -0500

    Update Heroe.java

--- Proyecto/src/Heroe.java
+++ Proyecto/src/Heroe.java
@@ -2,16 +2,15 @@ import java.util.Scanner;
 public class Heroe extends Personaje {
     public Heroe (String nombre, int HP, int MP, int ataque, int defensa, int velociadad ){
         super(nombre, HP, MP, ataque, defensa, velociadad);
+        inicializarHabilidades();
     } 
-    @Override
     public void realizarTurno(Batalla batalla){
         if(!vivo) return;
 
         Scanner sc = new Scanner(System.in);
         System.out.println("\n Turno de "+ nombre);
-        System.out.println("1. Atacar  2. Defender  3. Pasar");
+        System.out.println("1. Atacar  2. Defender  3. Usar habilidad  4. Pasar");
         int opcion = sc.nextInt();
-
         switch (opcion) {
     case 1:
         Personaje enemigo = batalla.seleccionarObjetivoEnemigo();
@@ -28,8 +27,42 @@ public class Heroe extends Personaje {
     default:
         System.out.println(nombre + " pasa su turno");
         break;
+    case 3:
+        if (habilidades.isEmpty()) {
+            System.out.println("No tienes habilidades.");
+            break;
+        }
+        for (int i = 0; i < habilidades.size(); i++) {
+            Habilidad h = habilidades.get(i);
+            System.out.println((i + 1) + ". " + h.getNombre() + " (MP " + h.getCostoMP() + ")");
+        }
+        System.out.print("Elige habilidad: ");
+        int eleccion = sc.nextInt();
+        if (eleccion >= 1 && eleccion <= habilidades.size()) {
+            Habilidad h = habilidades.get(eleccion - 1);
+            Personaje objetivo = h instanceof Curacion ? batalla.seleccionarObjetivoHeroe() : batalla.seleccionarObjetivoEnemigo();
+            if (objetivo != null) {
+                h.aplicar(this, objetivo);
+            }
+        }
+        break;
 }
 
 
     }
 }
+private List<Habilidad> habilidades = new ArrayList<>();
+private void inicializarHabilidades() {
+    if (nombre.equals("Jessica")) {
+        habilidades.add(new BolaDeFuego());
+        habilidades.add(new Curacion());
+    } else if (nombre.equals("Angelo")) {
+        habilidades.add(new Curacion());
+    } else if (nombre.equals("Yangus")) {
+        habilidades.add(new GolpePesado());
+    } else {
+        habilidades.add(new BolaDeFuego()); // Héroe principal
+    }
+}
+
+
